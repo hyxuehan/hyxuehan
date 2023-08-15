9ffe948cb783c683c80bc2228838865c3feb9a68
hyxuehan <113748852+hyxuehan@users.noreply.github.com>
2023-08-16 00:49:08 +0800

    Delete api.py

--- api.py
+++
@@ -1,7 +0,0 @@
-
-import requests
-url = 'http://apis.juhe.cn/springTravel/risk'
-key = {'key': '0e786d4f1e34835951f7975bd2be0912'}
-res = requests.post(url=url,data=key)
-result = res.json()
-print(result)
